1d0d778a7d9f535643407ab4b91d476954e39bc6
“mounir <mounirehab2@icloud.com>
2023-03-11 12:08:03 -0800

    will still modify

--- script.js
+++ script.js
@@ -1,14 +1,80 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCasedCharacters = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCasedCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numericCharacters = ["0123456789"];
+var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 
-// Write password to the #password input
+
+function generatePassword() {
+
+  var passwordLength = prompt("How long would you like your password to be? (Choose between 8 and 128 characters)");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Invalid password length. Please choose a number between 8 and 128.");
+    return "";
+
+  console.log (passwordLength);
+
+  var lowercase = confirm("Include lowercase characters?");
+  var uppercase = confirm("Include uppercase characters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special?");
+
+  if (!lowercase && !uppercase && !numeric && !special) {
+    alert("Please select at least one character type to include.");
+    return "";
+  }
+
+  var possibleChars = "";
+
+  if (lowercase && uppercase && numeric && special) {
+    possibleChars += [lowerCasedCharacters+upperCasedCharacters+numericCharacters+specialCharacters];
+  } else if (lowercase && uppercase && numeric) {
+    possibleChars += [lowerCasedCharacters+upperCasedCharacters+numericCharacters];
+  } else if (lowercase && uppercase && special) {
+    possibleChars += [lowerCasedCharacters+upperCasedCharacters+specialCharacters];
+  } else if (lowercase && numeric && special) {
+    possibleChars += [lowerCasedCharacters+numericCharacters+specialCharacters];
+  } else if (uppercase && numeric && special) {
+    possibleChars += [upperCasedCharacters+numericCharacters+specialCharacters];
+  } else if (lowercase && uppercase) {
+    possibleChars += [lowerCasedCharacters+upperCasedCharacters];
+  } else if (lowercase && numeric) {
+    possibleChars += [lowerCasedCharacters+numericCharacters];
+  } else if (lowercase && special) {
+    possibleChars += [lowerCasedCharacters+specialCharacters];
+  } else if (uppercase && numeric) {
+    possibleChars += [upperCasedCharacters+numericCharacters];
+  } else if (uppercase && special) {
+    possibleChars += [upperCasedCharacters+specialCharacters];
+  } else if (numeric && special) {
+    possibleChars += [numericCharacters+specialCharacters];
+  } else if (lowercase) {
+    possibleChars += [lowerCasedCharacters];
+  } else if (uppercase) {
+    possibleChars += [upperCasedCharacters];
+  } else if (numeric) {
+    possibleChars += [numericCharacters];
+  } else if (special) {
+    possibleChars += [specialCharacters];
+  }
+
+console.log (possibleChars);
+
+  var password = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    password += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
+  }
+  return password;
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+}
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
