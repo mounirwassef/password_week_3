963b48d3a6393337ecd4128f027279e24e4f9040
“mounir <mounirehab2@icloud.com>
2023-03-13 23:22:04 -0700

    final change

--- script.js
+++ script.js
@@ -3,17 +3,23 @@ var lowerCasedCharacters = ["abcdefghijklmnopqrstuvwxyz"];
 var upperCasedCharacters = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numericCharacters = ["0123456789"];
 var specialCharacters = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+var bothCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+console.log(lowerCasedCharacters);
+
+var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
   var passwordLength = prompt("How long would you like your password to be? (Choose between 8 and 128 characters)");
 
+  console.log(passwordLength);
+
+
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Invalid password length. Please choose a number between 8 and 128.");
     return "";
-
-  console.log (passwordLength);
+  }
 
   var lowercase = confirm("Include lowercase characters?");
   var uppercase = confirm("Include uppercase characters?");
@@ -27,28 +33,30 @@ function generatePassword() {
 
   var possibleChars = "";
 
+  console.log(possibleChars);
+
   if (lowercase && uppercase && numeric && special) {
-    possibleChars += [lowerCasedCharacters+upperCasedCharacters+numericCharacters+specialCharacters];
+    possibleChars += [lowerCasedCharacters + upperCasedCharacters + numericCharacters + specialCharacters];
   } else if (lowercase && uppercase && numeric) {
-    possibleChars += [lowerCasedCharacters+upperCasedCharacters+numericCharacters];
+    possibleChars += [lowerCasedCharacters + upperCasedCharacters + numericCharacters];
   } else if (lowercase && uppercase && special) {
-    possibleChars += [lowerCasedCharacters+upperCasedCharacters+specialCharacters];
+    possibleChars += [lowerCasedCharacters + upperCasedCharacters + specialCharacters];
   } else if (lowercase && numeric && special) {
-    possibleChars += [lowerCasedCharacters+numericCharacters+specialCharacters];
+    possibleChars += [lowerCasedCharacters + numericCharacters + specialCharacters];
   } else if (uppercase && numeric && special) {
-    possibleChars += [upperCasedCharacters+numericCharacters+specialCharacters];
+    possibleChars += [upperCasedCharacters + numericCharacters + specialCharacters];
   } else if (lowercase && uppercase) {
-    possibleChars += [lowerCasedCharacters+upperCasedCharacters];
+    possibleChars += [lowerCasedCharacters + upperCasedCharacters];
   } else if (lowercase && numeric) {
-    possibleChars += [lowerCasedCharacters+numericCharacters];
+    possibleChars += [lowerCasedCharacters + numericCharacters];
   } else if (lowercase && special) {
-    possibleChars += [lowerCasedCharacters+specialCharacters];
+    possibleChars += [lowerCasedCharacters + specialCharacters];
   } else if (uppercase && numeric) {
-    possibleChars += [upperCasedCharacters+numericCharacters];
+    possibleChars += [upperCasedCharacters + numericCharacters];
   } else if (uppercase && special) {
-    possibleChars += [upperCasedCharacters+specialCharacters];
+    possibleChars += [upperCasedCharacters + specialCharacters];
   } else if (numeric && special) {
-    possibleChars += [numericCharacters+specialCharacters];
+    possibleChars += [numericCharacters + specialCharacters];
   } else if (lowercase) {
     possibleChars += [lowerCasedCharacters];
   } else if (uppercase) {
@@ -59,8 +67,6 @@ function generatePassword() {
     possibleChars += [specialCharacters];
   }
 
-console.log (possibleChars);
-
   var password = "";
 
   for (var i = 0; i < passwordLength; i++) {
@@ -68,6 +74,8 @@ console.log (possibleChars);
   }
   return password;
 }
+
+console.log(password);
 
 function writePassword() {
   var password = generatePassword();
@@ -77,4 +85,3 @@ function writePassword() {
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-}
